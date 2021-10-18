b4fa79a1e6387f7601bcf87ca32192b3553f9156
DanielChungara <danielchungara1@gmail.com>
2021-10-18 17:33:30 -0300

    Ajax example.

---
+++ ajax/ajax.js
@@ -0,0 +1,62 @@
+$(function(){
+
+    var page = 1,
+        pagelimit = 10,
+        totalrecord = 0;
+
+    fetchData();
+
+    // handling the prev-btn
+    $(".prev-btn").click(function(){
+        if (page > 1) {
+            page--;
+            fetchData();
+        }
+        console.log("Prev Page: " + page);
+    });
+
+    // handling the next-btn
+    $(".next-btn").click(function(){
+        if (page * pagelimit < totalrecord) {
+            page++;
+            fetchData();
+        }
+        console.log("Next Page: " + page);
+    });
+
+    function fetchData() {
+        // ajax() method to make api calls
+        $.ajax({
+            url: "http://localhost:3001/api/products/page",
+            type: "GET",
+            data: {
+                page: page,
+                size: pagelimit
+            },
+            success: function(data) {
+
+                if (data.docs) {
+                    var dataArr = data.docs;
+                    console.log(dataArr);
+
+                    totalrecord = data.totalDocs;
+
+                    var html = "";
+                    for (var i = 0; i < dataArr.length; i++) {
+                        html += "<div class='sample-user'>"+
+                            "<h3>ID: " + dataArr[i]._id + "</h3>"+
+                            "<p>Price: " + dataArr[i].price + "</p>" +
+                            "</div>"+
+                            "<hr />";
+                    }
+                    $("#result").html(html);
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+            }
+        });
+    }
+});
